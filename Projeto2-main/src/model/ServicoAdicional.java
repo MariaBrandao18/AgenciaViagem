69ec521ebc7b0f5505aea432ff20f1367b634bc7
Mc Lovin <maria.r.brandao@sempreceub.com>
2025-06-23 23:38:08 -0300

    Tentando versionar

---
+++ Projeto2-main/src/model/ServicoAdicional.java
@@ -0,0 +1,100 @@
+
+package model;
+
+import java.util.ArrayList;
+
+public abstract class ServicoAdicional {
+	// variaveis
+	String nome;
+	double preco;
+	static ArrayList<ServicoAdicional> servicos = new ArrayList<ServicoAdicional>();
+	
+	// construtor
+	public ServicoAdicional(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+		servicos.add(this);
+	}
+	
+	
+	
+	//funcoes
+	
+	 public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public static ArrayList<ServicoAdicional> getServicos() {
+		return servicos;
+	}
+	public static void setServicos(ArrayList<ServicoAdicional> servicos) {
+		ServicoAdicional.servicos = servicos;
+	}
+	public void adicionarServico(ServicoAdicional servico) {
+	        servicos.add(servico);
+	    }
+	
+	
+	public void listarServicos() {
+		for (ServicoAdicional s : servicos) {
+			System.out.println(" Nome: " + s.nome +
+							   "Preco: " + s.preco);
+		}
+	}
+	
+	public void buscarServico(String servicoBuscado) {
+		for (ServicoAdicional s : servicos) {
+			if (s.nome.equals(servicoBuscado)) {
+				System.out.println(servicoBuscado + " foi encontrado!");
+				return;
+			}
+		}
+	}
+	
+	public void removerServico(String servicoRemovido) {
+		for (ServicoAdicional s : servicos) {
+			if (s.nome.equals(servicoRemovido)) {
+				System.out.println(servicoRemovido + " foi removido!");
+				servicos.remove(s);
+				return;
+			}
+		}
+	}
+}
+
+// Servicos adicionais
+class Translado extends ServicoAdicional{
+	public Translado(String nome, double preco) {
+		super(nome,preco);
+		servicos.add(this);
+	}
+}
+
+class Passeios extends ServicoAdicional{
+	public Passeios(String nome, double preco) {
+		super(nome,preco);
+		servicos.add(this);
+	}
+}
+
+class MotoristaParticular extends ServicoAdicional{
+	public MotoristaParticular(String nome, double preco) {
+		super(nome,preco);
+		servicos.add(this);
+	}
+}
+
+class AluguelCarro extends ServicoAdicional{
+	public AluguelCarro(String nome, double preco) {
+		super(nome,preco);
+		servicos.add(this);
+	}
+}
